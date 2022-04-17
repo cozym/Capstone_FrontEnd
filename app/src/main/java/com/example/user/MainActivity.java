0aaa8c05380c3ce67ec14d563a1bb589cc93d5b5
kyounggudev <homin222@naver.com>
2022-04-18 02:43:42 +0900

    Initial commit

---
+++ app/src/main/java/com/example/user/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mToolbar.setTitleTextColor(Color.WHITE);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+}
