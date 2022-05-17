f7e9a6b31a41c69bb6f14bda33b8eff44dadf364
starboard98 <55227905+starboard98@users.noreply.github.com>
2022-05-17 20:52:00 +0900

    Update add_group.java

--- app/src/main/java/com/example/user/add_group.java
+++ app/src/main/java/com/example/user/add_group.java
@@ -7,7 +7,17 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 public class add_group extends AppCompatActivity {
+
+    private final int GET_GALLERY_IMAGE=200;
+    private ImageView imageview;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,5 +28,21 @@ public class add_group extends AppCompatActivity {
         mToolbar.setTitleTextColor(Color.WHITE);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        imageview = (ImageView)findViewById(R.id.thumbnailButton);
+        imageview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent. setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            Uri selectedImageUri = data.getData();
+            imageview.setImageURI(selectedImageUri);
+        }
     }
 }
