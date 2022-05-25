96b1050fdd9b36dfa005c86a658f4065811a49cd
cozym <ddongyee99@daum.net>
2022-05-25 09:49:19 +0900

    로그인 초안

--- app/src/main/java/com/example/practicespace/WebViewActivity.java
+++ app/src/main/java/com/example/practicespace/WebViewActivity.java
@@ -12,14 +12,30 @@ import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.android.volley.toolbox.HttpResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Map;
+
+import okhttp3.Call;
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class WebViewActivity extends AppCompatActivity {
 
     private String TAG = WebViewActivity.class.getSimpleName();
+    String USER_AGENT = "Mozilla/5.0 (Linux; Android 4.1.1; Galaxy Nexus Build/JRO03C) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19";
 
     private WebView webView = null;
 
@@ -30,36 +46,73 @@ public class WebViewActivity extends AppCompatActivity {
 
         webView = (WebView) findViewById(R.id.webview);
 
-        webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient());
+        webView.setWebViewClient(new YourWebClient());
+        //webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setUserAgentString(USER_AGENT);
 
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.getSettings().setUseWideViewPort(true);
+        //webView.setWebChromeClient(new WebChromeClient());
 
-        webView.getSettings().setSupportZoom(false);
-        webView.getSettings().setBuiltInZoomControls(false);
+        //webView.getSettings().setLoadWithOverviewMode(true);
+        //webView.getSettings().setUseWideViewPort(true);
 
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        webView.getSettings().setSupportMultipleWindows(true);
+        //webView.getSettings().setSupportZoom(false);
+        //webView.getSettings().setBuiltInZoomControls(false);
 
-        webView.getSettings().setDomStorageEnabled(true);
+        //webView.getSettings().setJavaScriptEnabled(true);
+        //webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        //webView.getSettings().setSupportMultipleWindows(true);
+
+        //webView.getSettings().setDomStorageEnabled(true);
+
+
+        webView.clearCache(true);
+        webView.clearHistory();
 
         Log.d("tttttttttttttest", TAG);
 
-        /*
-        webView.setWebViewClient(new WebViewClient() {
-            @SuppressLint("NewApi")
-            @Override
-            public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceResponse response) {
-                Log.d("check", "response.getResponseHeaders()::"+response.getResponseHeaders());
 
-                return null;
-            }
-        });*/
-        webView.setWebViewClient(getWebViewClient());
-        webView.loadUrl("https://github.com/cozym/Capstone_FrontEnd/tree/master");  //test url
+        webView.loadUrl("http://5gradekgucapstone.xyz:8080/oauth2/authorization/google");  //test url
+
     }
+
+    private class YourWebClient extends WebViewClient {
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+        //Log.d("bbbbbbbbb",request.getRequestHeaders("content-type","text/plain").toString());
+        if (request.getUrl().toString().contains("aafwfw")) {// condition to intercept webview's request
+            return handleIntercept(request);
+        } else {
+            Log.d("eeeeeeeeelse", TAG);
+            return super.shouldInterceptRequest(view, request);
+            //return handleIntercept(request);
+        }
+    }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private WebResourceResponse handleIntercept(WebResourceRequest request){
+        OkHttpClient okHttpClient = new OkHttpClient();
+        final Call call = okHttpClient.newCall(new Request.Builder()
+                .url(request.getUrl().toString())
+                .method(request.getMethod(),null)
+                .headers(Headers.of(request.getRequestHeaders()))
+                .build()
+        );
+        try {
+            final Response response = call.execute();
+            response.headers();// get response header here
+            Log.d("aaaaaaaaaaaaaaaaaa", response.header("Authorization","text/plain").toString());
+            return new WebResourceResponse(
+                    response.header("content-type", "text/plain"), // You can set something other as default content-type
+                    response.header("content-encoding", "utf-8"),  //you can set another encoding as default
+                    response.body().byteStream()
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+    }
+
 
     private Map<String, String> getCustomHeaders()
     {
@@ -68,30 +121,10 @@ public class WebViewActivity extends AppCompatActivity {
         return headers;
     }
 
-    private WebViewClient getWebViewClient()
-    {
 
-        return new WebViewClient()
-        {
-
-            @Override
-            @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request)
-            {
-                view.loadUrl(request.getUrl().toString(), getCustomHeaders());
-                return true;
-            }
-
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url)
-            {
-                view.loadUrl(url, getCustomHeaders());
-                return true;
-            }
-        };
-    }
     /* public void GetUserInfo(String response) {
         Gson gson = new Gson();
         UserList userList = gson.fromJson(response, UserList.class);
     } */
+
 }
