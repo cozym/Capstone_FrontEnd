eb18388e871b071362f76610975c6eb9ed1bfd89
SeoulPolarBear <96487511+SeoulPolarBear@users.noreply.github.com>
2022-05-27 23:52:42 +0900

    Update APIClient.java
    add APICLient

--- app/src/main/java/com/example/capstone_frontend/APIClient.java
+++ app/src/main/java/com/example/capstone_frontend/APIClient.java
@@ -1,2 +1,18 @@
-package com.example.capstone_frontend;public class APIClient {
+package com.example.capstone_frontend;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APIClient {
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        OkHttpClient client = new OkHttpClient().newBuilder().build();
+        retrofit = new Retrofit.Builder()
+                .baseUrl("http://5gradekgucapstone.xyz:8080/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client).build();
+        return retrofit;
+    }
 }
