bc400d2c3125e8ac4252c2fbe3a90bf18e47548a
kyounggudev <101247033+kyounggudev@users.noreply.github.com>
2022-06-02 04:00:30 +0900

    Update Fragment1.java

--- app/src/main/java/com/example/practicespace/ui/Fragment1.java
+++ app/src/main/java/com/example/practicespace/ui/Fragment1.java
@@ -1,96 +1,84 @@
 package com.example.practicespace.ui;
 
-
 import android.content.Context;
-import android.os.Bundle;
+import android.content.Intent;
+import android.os.Parcelable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
-
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.practicespace.R;
-import com.example.practicespace.connection.APIClient;
-import com.example.practicespace.connection.APIInterface;
-import com.example.practicespace.connection.openGroupList;
-import com.example.practicespace.vo.Group;
 
 import java.util.ArrayList;
-import java.util.List;
 
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+public class ListViewAdapter extends ArrayAdapter<ListViewItem>{
+    //각 리스트에 들어갈 아이템들을 가지고 있는 리스트 배열
+    private ArrayList<ListViewItem> listViewItemList;
+    Context mContext;
+    //position에 위치한 데이터를 화면에 출력하는데 사용될 view를 리턴, : 필수 구현
+    //view는 넣을 곳 즉, viewgroup에 들어가 view 생성
+    //viewgroup은 내가 보고 있는 화면 즉, list
 
-public class Fragment1 extends Fragment {
-
-    ListView listview;
-    private static ListViewAdapter adapter;
-
-    APIInterface apiInterface = APIClient.getClient().create(APIInterface.class);
-    List<Group> groups = new ArrayList<Group>();
-
-    @Override
-    public void onAttach(Context context) {
-        Log.d("123123","123123");
-
-        super.onAttach(context);
-        Log.d("321321","3212321");
+    //View lookup cache
+    private static class ViewHolder{
+        ImageView ICon;
+        TextView text_Group_Name;
+        TextView text_Hash_Tag;
     }
 
+    public ListViewAdapter(ArrayList<ListViewItem> list, Context context){
+        super(context, R.layout.listview_crew_item, list);
+        this.listViewItemList = list;
+        this.mContext = context;
+    }
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        getGroupList();
+    public View getView(int position, View convertView, ViewGroup parent) {
+        //position에 위치시키기 위해 listViewItem 하나 생성
+        ListViewItem listViewItem = getItem(position);
+        //cache 사용
+        ViewHolder viewHolder;
 
-        //adapter에 넣을 리스트뷰를 받는 배열
-        ArrayList<ListViewItem> items = new ArrayList<ListViewItem>();
+        if(convertView == null){
+            viewHolder = new ViewHolder();
+            LayoutInflater inflater = LayoutInflater.from(getContext());
 
-        for(int i = 0; i <groups.size(); i++){
-            items.add(new ListViewItem(R.drawable.profile,groups.get(i).getName(), "#캡스톤", groups.get(i).getSeq()));
+//사용항 inflater의 객체 얻는 3가지 방법
+//LayoutInflater inflater =
+//(LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+//LayoutInflater inflater = Activity.getLayoutInflater();
+//LayoutInflater inflater = LayoutInflater.from(Context context);
+            convertView = inflater.inflate(R.layout.listview_crew_item,parent,false);//resourceId는 R.id.listView_crew_item이다.
+            viewHolder.ICon = (ImageView) convertView.findViewById(R.id.group_icon);
+            viewHolder.text_Group_Name = (TextView)convertView.findViewById(R.id.group_name);
+            viewHolder.text_Hash_Tag = (TextView)convertView.findViewById(R.id.hash_tag);
 
+            convertView.setTag(viewHolder);
+        }
+        else{
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
-//        items.add(new ListViewItem(R.drawable.profile, "5학년 다니면 그만이야", "#캡스톤"));
-//        items.add(new ListViewItem(R.drawable.profile, "영통구 책방", "#캡스톤"));
-//        items.add(new ListViewItem(R.drawable.profile, "경기대 도서관", "#캡스톤"));
-//        items.add(new ListViewItem(R.drawable.profile, "컴공책 동아리", "#캡스톤"));
-//        items.add(new ListViewItem(R.drawable.profile, "논문만 판다", "#캡스톤"));
-//        items.add(new ListViewItem(R.drawable.profile, "공대생 모여라", "#캡스톤"));
-//        items.add(new ListViewItem(R.drawable.profile, "경기도 모여라", "#캡스톤"));
-//        items.add(new ListViewItem(R.drawable.profile, "서울도 모여라", "#캡스톤"));
+        //아이템 내 각 위젯에 대한 데이터 반영
+        //위젯에 내가 만들어 놓은 부분 적용
+        viewHolder.ICon.setImageResource(listViewItem.getIcon());
+        viewHolder.text_Group_Name.setText(listViewItem.getGroupName());
+        String group_date = listViewItem.getCreatedDate().substring(0,10);
+        //클릭이벤트
+        LinearLayout cmdArea = (LinearLayout)convertView.findViewById(R.id.group_click);
+        Intent intent = new Intent(getContext(), group_enter.class);
+        intent.putExtra("그룹이름",viewHolder.text_Group_Name.getText());
+        intent.putExtra("그룹설명",listViewItem.getGroupDes());
+        intent.putExtra("그룹공개",listViewItem.getIsOpen());
+//        intent.putExtra("관리자", listViewItem.getAdmin().getNickname());
+        intent.putExtra("그룹시퀀스",listViewItem.getGroupSeq());
+        intent.putExtra("그룹생성일" ,group_date);
+        cmdArea.setOnClickListener(v -> getContext().startActivity(intent));
 
-        View view = inflater.inflate(R.layout.frag_group, container, false);
-        listview = (ListView) view.findViewById(R.id.List_group_Info);
-
-        adapter = new ListViewAdapter(items, view.getContext());
-
-        listview.setAdapter(adapter);
-
-        return view;
-    }
-
-    public void getGroupList(){
-        Call<openGroupList> call = apiInterface.getGroupList();
-
-        call.enqueue(new Callback<openGroupList>() {
-            @Override
-            public void onResponse(Call<openGroupList> call, Response<openGroupList> response) {
-                openGroupList result = response.body();
-                if(response.code() == 200){
-                    groups = result.data.groups;
-
-                } else{
-                    Log.d("연결 테스트", "실패");
-                }
-            }
-
-            @Override
-            public void onFailure(Call<openGroupList> call, Throwable t) {
-                call.cancel();
-            }
-        });
+        return convertView;
     }
 }
