1b846f1a3475329abf1c1b56d0fd88b5ce462228
kyounggudev <101247033+kyounggudev@users.noreply.github.com>
2022-06-02 03:59:04 +0900

    Update ListViewItem.java
    파라미터 추가

--- app/src/main/java/com/example/practicespace/ui/ListViewItem.java
+++ app/src/main/java/com/example/practicespace/ui/ListViewItem.java
@@ -1,18 +1,26 @@
 package com.example.practicespace.ui;
 
+import com.example.practicespace.vo.Admin;
+
 public class ListViewItem {
 
     private int group_icon;
     private String group_name;
-    private String hash_tag;
+    private String description;
     private int groupSeq;
+    private boolean isOpen;
+    private String createdDate;
+    private Admin admin;
 
-    public ListViewItem(int icon, String name, String hashtag, int seq){
+
+    public ListViewItem(int icon, String name, String description, int seq, boolean isOepn, String createdDate, Admin admin){
         this.group_icon = icon;
         this.group_name = name;
-        this.hash_tag = hashtag;
+        this.description = description;
         this.groupSeq = seq;
-
+        this.isOpen = isOepn;
+        this.createdDate = createdDate;
+        this.admin = admin;
     }
 
 
@@ -20,7 +28,9 @@ public class ListViewItem {
 
     public int getIcon(){ return this.group_icon; }
     public String getGroupName(){ return this.group_name; }
-    public String getHashTag(){ return this.hash_tag; }
     public int getGroupSeq(){ return this.groupSeq; }
-
+    public boolean getIsOpen(){return this.isOpen;}
+    public String getGroupDes(){return this.description;}
+    public String getCreatedDate(){return this.createdDate;}
+    public Admin getAdmin(){return  this.admin;}
 }
